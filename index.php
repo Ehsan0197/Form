92d3428699e0e6037d76a24b99d4c0b09b28cac5
Ehsan Darabi <ehsandarabi27@gmail.com>
2022-08-28 15:13:42 +0430

    form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> FORM </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="index2.php" method="post">
+            <h3>Name: <input type="text" name="name" required="required">
+            <br><br>
+            Last Name: <input type="text" name="last_name" required="required">
+            <br><br>
+            Email: <input type="email" name="email" required="required">
+            <br><br>
+            <input type="submit" value="Create"></h3>
+        </form>
+    </body>
+</html>
